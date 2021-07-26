aff1d9f70832f17546744e3c338a05be471a2205
skylogin <skylogin@gmail.com>
2021-07-26 22:16:38 +0900

    graphql api 연결

--- web/src/App.js
+++ web/src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+require('dotenv').config()
 
 import GlobalStyle from './components/GlobalStyle';
 
 import Pages from './pages';
 
+const uri = process.env.API_URI;
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  uri,
+  cache,
+  connectToDevTools: true
+});
+
 const App = () => {
   return (
-    <div>
+    <ApolloProvider client={client}>
       <GlobalStyle />
       <Pages />
-    </div>
+    </ApolloProvider>
   )
 }
 
--- web/src/pages/home.js
+++ web/src/pages/home.js
@@ -1,12 +1,42 @@
 import React from 'react';
+import { useQuery, gql } from '@apollo/client';
 
 import Button from '../components/Button';
 
+const GET_NOTES = gql`
+  query noteFeed($cursor: String) {
+    noteFeed(cursor: $cursor){
+      cursor
+      hasNextPage
+      notes{
+        id
+        createdAt
+        content
+        favoriteCount
+        author{
+          username
+          id
+          avatar
+        }
+      }
+    }
+  }
+`;
+
 const Home = () => {
+  const { data, loading, error, fetchMore } = useQuery(GET_NOTES);
+
+  if(loading){
+    return <p>Loading...</p>
+  }
+  if(error){
+    return <p>Error!</p>
+  }
+
   return (
     <div>
-      <p>This is the home page</p>
-      <Button>Click Me!</Button>
+      {console.log(data)}
+      The data loaded!
     </div>
   )
 }
