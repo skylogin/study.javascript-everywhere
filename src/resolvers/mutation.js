1b8bf098c85da4b7cceaf755e70d4b683d793dbf
skylogin <skylogin@gmail.com>
2021-07-26 17:22:53 +0900

    가입, 로그인에 대한 graphql 적용

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -1,3 +1,13 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const {
+  AuthenticationError, 
+  ForbiddenError
+} = require('apollo-server-express');
+require('dotenv').config();
+
+const gravatar = require('../util/gravatar');
+
 module.exports = {
   newNote: async (parent, args, { models }) => {
     return await models.Note.create({
@@ -28,4 +38,37 @@ module.exports = {
       }
     );
   },
+  signUp: async(parent, {username, email, password}, { models }) => {
+    email = email.trim().toLowerCase();
+    const hashed = await bcrypt.hash(password, 10);
+    const avatar = gravatar(email);
+    try{
+      const user = await models.User.create({ username, email, avatar, password: hashed});
+
+      return jwt.sign({id: user._id }, process.env.JWT_SECRET);
+    } catch(err){
+      console.log(err);
+      throw new Error('Error creating account');
+    }
+  },
+  signIn: async (parent, {username, email, password}, { models }) => {
+    if(email){
+      email = email.trim().toLowerCase();
+    }
+
+    const user = await models.User.findOne({
+      $or: [{ email }, { username }]
+    });
+
+    if(!user){
+      throw new AuthenticationError('erro signing in');
+    }
+
+    const valid = await bcrypt.compare(password, user.password);
+    if(!valid){
+      throw new AuthenticationError('erro signing in');
+    }
+
+    return jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+  }
 };
