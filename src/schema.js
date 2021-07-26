9f60b298a6ee9f2e9e2148a4acdd61a0f0872e51
skylogin <skylogin@gmail.com>
2021-07-26 17:12:53 +0900

    user 추가

--- src/schema.js
+++ src/schema.js
@@ -1,7 +1,7 @@
 const { gql } = require('apollo-server-express');
 
 module.exports = gql`
-  scalar DateTime
+  scalar DateTime,
   type Note {
     id: ID!
     content: String!
@@ -9,11 +9,20 @@ module.exports = gql`
     createdAt: DateTime!
     updatedAt: DateTime!
   },
+  type User{
+    id: ID!
+    username: String!
+    email: String!
+    avatar: String
+    notes: [Note!]!
+  },
   type Mutation{
     newNote(content: String!): Note!
     updateNote(id: ID!, content: String!): Note!
     deleteNote(id: ID!): Boolean!
-  }
+    signUp(username: String!, email: String!, password: String!): String!
+    signIn(username: String, email: String, password: String!): String!
+  },
   type Query {
     hello: String
     notes: [Note!]!
