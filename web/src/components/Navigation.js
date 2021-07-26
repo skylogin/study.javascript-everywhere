ca1880ea8714a18b31f12b189e4d1c3747b76a17
skylogin <skylogin@gmail.com>
2021-07-26 21:51:41 +0900

    css 적용

--- web/src/components/Navigation.js
+++ web/src/components/Navigation.js
@@ -1,21 +1,71 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Nav = styled.nav`
+  padding: 1em;
+  background: #f5f4f0;
+
+  @media (max-width: 700px) {
+    padding-top: 64px;
+  }
+
+  @media (min-width: 700px) {
+    position: fixed;
+    width: 220px;
+    height: calc(100% - 64px);
+    overflow-y: scroll;
+  }
+`;
+
+const NavList = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  line-height: 2;
+
+  a {
+    text-decoration: none;
+    font-weight: bold;
+    font-size: 1.1em;
+    color: #333;
+  }
+
+  a:visited {
+    color: #333;
+  }
+
+  a:hover,
+  a:focus {
+    color: #0077cc;
+  }
+`;
+
 
 const Navigation = () => {
   return (
-    <nav>
-      <ul>
-       <li>
-          <Link to="/">Home</Link>
+    <Nav>
+      <NavList>
+        <li>
+          <span aria-hidden="true" role="img">
+            🏠
+          </span>
+          <Link to="/"> Home</Link>
         </li>
         <li>
-          <Link to="/mynotes">My Notes</Link>
+          <span aria-hidden="true" role="img">
+            📓 
+          </span>
+          <Link to="/mynotes"> My Notes</Link>
         </li>
         <li>
-          <Link to="/favorites">Favorites</Link>
+          <span aria-hidden="true" role="img">
+            🌟
+          </span>
+          <Link to="/favorites"> Favorites</Link>
         </li>
-      </ul>
-    </nav>
+      </NavList>
+    </Nav>
   )
 }
 
