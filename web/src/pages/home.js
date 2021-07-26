a4ed389b59268ca08f6aa040e69b57c308a3f512
skylogin <skylogin@gmail.com>
2021-07-26 21:38:12 +0900

    공통 컴포넌트 개발

--- web/src/pages/home.js
+++ web/src/pages/home.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Header from '../components/Header';
+import Navigation from '../components/Navigation';
 
 const Home = () => {
   return (
     <div>
-      <h1>Notedly</h1>
+      <Header />
+      <Navigation />
       <p>This is the home page</p>
     </div>
   )
