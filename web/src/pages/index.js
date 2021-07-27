cb14d6a19688986ec36ead87a742c60a46a47569
skylogin <skylogin@gmail.com>
2021-07-27 12:48:49 +0900

    로그인유저만 볼수있는 페이지 권한 추가

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { useQuery, gql } from '@apollo/client';
 
 import Layout from '../components/Layout';
 
@@ -10,13 +11,49 @@ import NotePage from './note';
 import SignUp from './signup';
 import SignIn from './signin';
 
+
+const IS_LOGGED_IN = gql`
+  {
+    isLoggedIn @client
+  }
+`;
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const { loading, error, data } = useQuery(IS_LOGGED_IN);
+
+  if(loading){
+    return <p>Loading...</p>;
+  }
+  if(error){
+    return <p>Error</p>;
+  }
+
+  return (
+    <Route
+      {...rest}
+      render={props => 
+        data.isLoggedIn === true? (
+          <Component {...props} />
+        ): (
+          <Redirect
+            to={{
+              pathname: '/signin',
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    />
+  )
+}
+
 const Pages = () => {
   return (
     <Router>
       <Layout>
         <Route exact path="/" component={Home} />
-        <Route path="/mynotes" component={MyNotes} />
-        <Route path="/favorites" component={Favorites} />
+        <PrivateRoute path="/mynotes" component={MyNotes} />
+        <PrivateRoute path="/favorites" component={Favorites} />
         <Route path="/note/:id" component={NotePage} />
         <Route path="/signup" component={SignUp} />
         <Route path="/signin" component={SignIn} />
