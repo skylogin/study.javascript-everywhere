471e1216307297b031e26c4dc65017eab3026758
skylogin <skylogin@gmail.com>
2021-07-27 12:40:46 +0900

    가입/로그인폼 공통컴포넌트 추출 및 로그인구현

--- web/src/pages/signin.js
+++ web/src/pages/signin.js
@@ -3,36 +3,36 @@ import { useMutation, useApolloClient, gql } from '@apollo/client';
 
 import UserForm from '../components/UserForm';
 
-const SIGNUP_USER = gql`
-  mutation signUp($email: String!, $username: String!, $password: String!){
-    signUp(email: $email, username: $username, password: $password)
+const SIGNIN_USER = gql`
+  mutation signIn($email: String!, $password: String!){
+    signIn(email: $email, password: $password)
   }
 `;
 
 
-const SignUp = props => {
+const SignIn = props => {
   const client = useApolloClient();
 
   useEffect(() => {
-    document.title = 'Sign Up - Notedly';
+    document.title = 'Sign In - Notedly';
   });
   
-  const [signUp, { loading, error }] = useMutation(SIGNUP_USER, {
+  const [signIn, { loading, error }] = useMutation(SIGNIN_USER, {
     onCompleted: data => {
-      localStorage.setItem('token', data.signUp);
+      localStorage.setItem('token', data.signIn);
       client.writeData({ data: { isLoggedIn: true }});
       props.history.push('/');
     }
   });
 
-
+  
   return (
     <React.Fragment>
-      <UserForm action={signUp} formType="signup" />
+      <UserForm action={signIn} formType="signin" />
       {loading && <p>Loading...</p>}
       {error && <p>Error creating an account</p>}
     </React.Fragment>
   )
 }
 
-export default SignUp;
+export default SignIn;
