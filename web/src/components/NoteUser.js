71a3ac6efb11bb13f83b4ac85e8a8b237856271b
skylogin <skylogin@gmail.com>
2021-07-27 19:13:40 +0900

    즐겨찾기 추가

--- web/src/components/NoteUser.js
+++ web/src/components/NoteUser.js
@@ -3,7 +3,9 @@ import { useQuery } from '@apollo/client';
 import { Link } from 'react-router-dom';
 
 import { GET_ME } from '../gql/query';
+
 import DeleteNote from './DeleteNote';
+import FavoriteNote from './FavoriteNote';
 
 
 const NoteUser = props => {
@@ -17,7 +19,11 @@ const NoteUser = props => {
 
   return (
     <React.Fragment>
-      Favorites: {props.note.favoriteCount}
+      <FavoriteNote
+        me={data.me}
+        noteId={props.note.id}
+        favoriteCount={props.note.favoriteCount}
+      />
       <br/>
       {data.me.id === props.note.author.id && (
         <React.Fragment>
